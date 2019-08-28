7d04044bb04ef734cf8ee0405a7181aab9b30d9e
kloudtrader-github <github@kloudtrader.com>
2019-08-28 10:30:20 +0000

    commit

--- libkloudtrader/logs.py
+++ libkloudtrader/logs.py
@@ -1,5 +1,5 @@
 import logging
-import coloredlogs
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -9,47 +9,8 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-coloredlogs.DEFAULT_FIELD_STYLES = {'asctime': {'color': 'green'}}
-coloredlogs.DEFAULT_LEVEL_STYLES = {
-    'critical': {
-        'color': 'red'
-    },
-    'debug': {
-        'color': 'white'
-    },
-    'error': {
-        'color': 'red',
-        'bold': True
-    },
-    'info': {
-        'color': 'cyan',
-        'bold': True
-    },
-    'notice': {
-        'color': 'magenta',
-        'bold': True
-    },
-    'spam': {
-        'color': 'green',
-        'faint': True
-    },
-    'success': {
-        'color': 'green',
-        'bold': True
-    },
-    'verbose': {
-        'color': 'blue'
-    },
-    'warning': {
-        'color': 'yellow',
-        'bold': True
-    }
-}
-'''
-coloredlogs.install(fmt="%(asctime)s: %(message)s",
-                    datefmt='%A %B %d, %Y %I:%M:%S %p',
-                    level="INFO")
-'''
+
+
 logging.basicConfig(format=bcolors.WARNING+bcolors.BOLD+"%(asctime)s %(levelname)s: %(message)s",
                     datefmt='%A %B %d, %Y %I:%M:%S %p',
                     level="INFO")
